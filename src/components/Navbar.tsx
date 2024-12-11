9a26d3607a476b108274638950985cb2d65013d6
ayameeeya <aaa.zi35@outlook.com>
2024-12-11 15:08:40 +0900

    feat: fix mobile glass-effect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -57,7 +57,7 @@ const Navbar = () => {
       </div>
 
       {isOpen && (
-        <div className="md:hidden bg-gray-900/5 backdrop-blur-lg border border-gray-200/20 mt-2">
+        <div className="md:hidden bg-white/80 backdrop-blur-lg mt-2">
           <div className="px-4 py-4 space-y-3">
             <Link
               to="/#services"
